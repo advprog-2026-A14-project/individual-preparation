58b6b42814eb660cc5bb68fa5239796f17457f3a
HeraldoArman <heraldoarman7@gmail.com>
2026-02-10 22:35:00 +0700

    feat: create vector norm method

--- src/main/java/com/example/individualprep/service/VectorUtility.java
+++ src/main/java/com/example/individualprep/service/VectorUtility.java
@@ -4,29 +4,35 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class VectorUtility {
-    
+
     public double[] add(double[] v1, double[] v2) {
         // TODO: Implement me properly!
-        return new double[] { 0.0, 0.0, 0.0 };
+        return new double[]{0.0, 0.0, 0.0};
     }
 
     public double[] subtract(double[] v1, double[] v2) {
         // TODO: Implement me properly!
-        return new double[] { 0.0, 0.0, 0.0 };
+        return new double[]{0.0, 0.0, 0.0};
     }
 
     public double[] multiply(double[] v1, int x) {
         // TODO: Implement me properly!
-        return new double[] { 0.0, 0.0, 0.0 };
+        return new double[]{0.0, 0.0, 0.0};
     }
-    
+
     public double dotProduct(double[] v1, double[] v2) {
         // TODO: Implement me properly!
         return 0.0;
     }
-    
+
     public double norm(double[] v1) {
-        // TODO: Implement me properly!
-        return 0.0;
+        if (v1 == null) {
+            throw new IllegalArgumentException("Vector cannot be null");
+        }
+        double sum = 0.0;
+        for (double val : v1) {
+            sum += val * val;
+        }
+        return Math.sqrt(sum);
     }
 }
