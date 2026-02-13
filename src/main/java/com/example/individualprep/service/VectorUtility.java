57831044ac8115176f6533b24d3b60546e65a67c
rizkyantariksa <rizkyantariksa02@gmail.com>
2026-02-13 10:18:18 +0700

    feat: create vector-dotproduct

--- src/main/java/com/example/individualprep/service/VectorUtility.java
+++ src/main/java/com/example/individualprep/service/VectorUtility.java
@@ -21,12 +21,25 @@ public class VectorUtility {
     }
     
     public double dotProduct(double[] v1, double[] v2) {
-        // TODO: Implement me properly!
-        return 0.0;
+        validatePair(v1, v2);
+        double result = 0.0;
+        for (int i = 0; i < v1.length; i++) {
+            result += v1[i] * v2[i];
+        }
+        return result;
     }
     
     public double norm(double[] v1) {
         // TODO: Implement me properly!
         return 0.0;
+    }
+
+    private void validatePair(double[] v1, double[] v2) {
+        if (v1 == null || v2 == null) {
+            throw new IllegalArgumentException("Vectors cannot be null");
+        }
+        if (v1.length != v2.length) {
+            throw new IllegalArgumentException("Vectors must have the same length");
+        }
     }
 }
