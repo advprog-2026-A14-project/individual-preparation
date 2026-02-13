af09a22618750a3a9eb936b6c09147e3d34e5acf
anggarabayuadji <a.anggara.bayuadji.p@gmail.com>
2026-02-13 08:01:10 +0700

    created vector subtract and its tests

--- src/test/java/com/example/individualprep/service/VectorUtilityTest.java
+++ src/test/java/com/example/individualprep/service/VectorUtilityTest.java
@@ -42,4 +42,52 @@ class VectorUtilityTest {
         vectorUtility.norm(v);
         assertArrayEquals(copy, v, 0.0);
     }
+
+    @Test
+    void testSubtractPositiveValue() {
+        double[] v1 = {3.0, 4.0, 1.0};
+        double[] v2 = {1.0, 2.0, 7.0};
+        double[] expected1 = {2.0, 2.0, -6.0};
+
+        assertArrayEquals(expected1, vectorUtility.subtract(v1, v2), 1e-10);
+
+        double[] v3 = {0.3, 0.4, 0.1};
+        double[] v4 = {0.2, 0.1, 0.2};
+        double[] expected2 = {0.1, 0.3, -0.1};
+
+        assertArrayEquals(expected2, vectorUtility.subtract(v3, v4), 1e-10);
+    }
+
+    @Test
+    void testSubtractNegativeValue() {
+        double[] v1 = {-3.0, -4.0, -1.0};
+        double[] v2 = {-1.0, -2.0, -7.0};
+        double[] expected1 = {-2.0, -2.0, 6.0};
+
+        assertArrayEquals(expected1, vectorUtility.subtract(v1, v2), 1e-10);
+
+        double[] v3 = {-0.3, -0.4, -0.1};
+        double[] v4 = {-0.2, -0.1, -0.2};
+        double[] expected2 = {-0.1, -0.3, 0.1};
+
+        assertArrayEquals(expected2, vectorUtility.subtract(v3, v4), 1e-10);
+    }
+
+    @Test
+    void testSubtractMixedSign() {
+        double[] v1 = {-0.3, -0.4, 0.1};
+        double[] v2 = {0.2, -0.1, -0.2};
+        double[] expected1 = {-0.5, -0.3, 0.3};
+
+        assertArrayEquals(expected1, vectorUtility.subtract(v1, v2), 1e-10);
+    }
+
+    @Test
+    void testSubtractPrecision() {
+        double[] v1 = {1_000_000.0, 0.0000001};
+        double[] v2 = {1.0, 0.00000005};
+        double[] expected = {999_999.0, 0.00000005};
+
+        assertArrayEquals(expected, vectorUtility.subtract(v1, v2), 1e-10);
+    }
 }
